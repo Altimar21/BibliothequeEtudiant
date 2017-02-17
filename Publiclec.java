12680b51904702938d5df643c11d5e60648926c3
A-Julien <julien.alaimo@gmail.com>
2017-02-17 13:09:15 +0100

    Affichage apres creation du book

--- Publiclec.java
+++ Publiclec.java
@@ -3,7 +3,18 @@
  * Package name : PACKAGE_NAME.
  */
 public enum Publiclec {
-    ENFANT,
-    ADO,
-    ADULTE
-};
+    ENFANT ("Enfant"),
+    ADO ("ADO"),
+    ADULTE ("ADULTE");
+
+    private String name = "";
+
+        //Constructeur
+    Publiclec(String name){
+        this.name = name;
+    }
+
+    public String toString(){
+            return name;
+    }
+}
